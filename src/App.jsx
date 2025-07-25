6fe859f748e4ca9a66e595a011056a61d6540091
MrrClaude <nhemsoksophorn@gmail.com>
2025-07-25 14:42:55 +0700

    first push

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Navbar from "./Components/Navbar";
+import Footer from "./Components/footer";
+
+const App = () => {
+  return (
+    <div>
+      <nav>
+        <Navbar />
+      </nav>
+      <main></main>
+      <footer>
+        <Footer />
+      </footer>
+    </div>
+  );
+};
+
+export default App;
